ca49c3f25ca63663086995303920813a1e288ecd
Gabriel Vancini <gabriel.vancini@climatempo.io>
2022-03-04 19:00:21 -0300

    fix: post id method correction

--- pages/index.js
+++ pages/index.js
@@ -22,14 +22,14 @@ export default function Home({ allPostsData }) {
       </Head>
       <section className={utilStyles.headingMd}>
         <p>Olá, eu sou o Gabriel. Sou um estudante de Ciências da Computação e esse é meu estudo sobre Next.js</p>
-        
+
       </section>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={'/posts/${id}'}>
+              <Link href={`/posts/${id}`}>
                 <a>{title}</a>
               </Link>
               <br />
